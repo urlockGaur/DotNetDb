930a98b42acad6f4308bbea7e405c54431400450
JEFFREY GRISSOM <jeff@JEFFREYs-iMac.local>
2020-08-09 21:08:33 -0500

    Update YourName application - input name - display name

--- YourName/Program.cs
+++ YourName/Program.cs
@@ -6,7 +6,13 @@ namespace YourName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // display a literal value
+            Console.WriteLine("What is your name?");
+            // input a value and assign it to a string variable
+            string name = Console.ReadLine();
+            // display the string variable
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hello, {0}", name);
         }
     }
 }
