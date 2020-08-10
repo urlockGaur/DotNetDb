6dbf25a1954f5af7923edcd759400a72c31d28cf
JEFFREY GRISSOM <jeff@JEFFREYs-iMac.local>
2020-08-09 22:07:50 -0500

    Update CourseGPAFile application - use an indefinite loop to create options - input user choice

--- CourseGPAFile/Program.cs
+++ CourseGPAFile/Program.cs
@@ -6,7 +6,25 @@ namespace CourseGPAFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string choice;
+            do
+            {
+                // ask user a question
+                Console.WriteLine("1) Read data from file.");
+                Console.WriteLine("2) Create file from data.");
+                Console.WriteLine("Enter any other key to exit.");
+                // input response
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    // TODO: read data from file
+                }
+                else if (choice == "2")
+                {
+                    // TODO: create file from data
+                }
+            } while (choice == "1" || choice == "2");
         }
     }
 }
