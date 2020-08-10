0fe7444cb43132ef6ae78bb122f1e7d3c524e307
JEFFREY GRISSOM <jeff@JEFFREYs-iMac.local>
2020-08-09 22:15:16 -0500

    Update CourseGPAFile application - implement option 1 - first ensure that file exists - read course info from text file using StreamReader class

--- CourseGPAFile/Program.cs
+++ CourseGPAFile/Program.cs
@@ -20,7 +20,33 @@ namespace CourseGPAFile
 
                 if (choice == "1")
                 {
-                    // TODO: read data from file
+                    // read data from file
+                    if (File.Exists(file))
+                    {
+                        // accumulators needed for GPA
+                        int gradePoints = 0;
+                        int count = 0;
+                        // read data from file
+                        StreamReader sr = new StreamReader(file);
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            // convert string to array
+                            string[] arr = line.Split('|');
+                            // display array data
+                            Console.WriteLine("Course: {0}, Grade: {1}", arr[0], arr[1]);
+                            // add to accumulators
+                            gradePoints += arr[1] == "A" ? 4 : arr[1] == "B" ? 3 : arr[1] == "C" ? 2 : arr[1] == "D" ? 1 : 0;
+                            count += 1;
+                        }
+                        // calculate GPA
+                        double GPA = (double)gradePoints / count;
+                        Console.WriteLine("GPA: {0:N2}", GPA);
+                    }
+                    else
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
                 }
                 else if (choice == "2")
                 {
