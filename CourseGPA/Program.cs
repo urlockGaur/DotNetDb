914e8692e1c139b5fbd4a6be7ab3928b690ec755
JEFFREY GRISSOM <jeff@JEFFREYs-iMac.local>
2020-08-09 21:22:20 -0500

    Create CourseGPA console application - add CourseGPA folder - dotnet new console - dotnet run

---
+++ CourseGPA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseGPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
