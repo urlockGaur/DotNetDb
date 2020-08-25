3c28cff7bd9969fe413ccad5b15bdcd96db32583
jgrissom <jgrissom91@gmail.com>
2020-08-25 14:21:06 -0500

    SleepData - log error for invalid int entered

--- SleepData/Program.cs
+++ SleepData/Program.cs
@@ -26,40 +26,47 @@ namespace SleepData
             {
                 // create data file
 
-                 // ask a question
+                // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
-                int weeks = int.Parse(Console.ReadLine());
-
-                 // determine start and end date
-                DateTime today = DateTime.Now;
-                // we want full weeks sunday - saturday
-                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-                // subtract # of weeks from endDate to get startDate
-                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-                
-                // random number generator
-                Random rnd = new Random();
-
-                // create file
-                StreamWriter sw = new StreamWriter(file);
-                // loop for the desired # of weeks
-                while (dataDate < dataEndDate)
+                string ans = Console.ReadLine();
+                int weeks;
+                if (!int.TryParse(ans, out weeks))
                 {
-                    // 7 days in a week
-                    int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
-                    {
-                        // generate random number of hours slept between 4-12 (inclusive)
-                        hours[i] = rnd.Next(4, 13);
-                    }
-                    // M/d/yyyy,#|#|#|#|#|#|#
-                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-                    // add 1 week to date
-                    dataDate = dataDate.AddDays(7);
+                    logger.Error("Invalid input (integer): {Answer}", ans);
                 }
-                sw.Close();
+                else
+                {
+                    // determine start and end date
+                    DateTime today = DateTime.Now;
+                    // we want full weeks sunday - saturday
+                    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                    // subtract # of weeks from endDate to get startDate
+                    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                    
+                    // random number generator
+                    Random rnd = new Random();
+
+                    // create file
+                    StreamWriter sw = new StreamWriter(file);
+                    // loop for the desired # of weeks
+                    while (dataDate < dataEndDate)
+                    {
+                        // 7 days in a week
+                        int[] hours = new int[7];
+                        for (int i = 0; i < hours.Length; i++)
+                        {
+                            // generate random number of hours slept between 4-12 (inclusive)
+                            hours[i] = rnd.Next(4, 13);
+                        }
+                        // M/d/yyyy,#|#|#|#|#|#|#
+                        //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                        // add 1 week to date
+                        dataDate = dataDate.AddDays(7);
+                    }
+                    sw.Close();
+                }
             }
             else if (resp == "2")
             {
